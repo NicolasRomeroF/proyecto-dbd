13860195ef133250c82c43e9aa016a1ffc2c0356
juanye <juan.barrerab@usach.cl>
2017-11-02 16:49:54 -0300

    LLamados a seeders ordenados según migraciones

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,30 +11,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-    	//$this->call(RolSeeder::class);
-    	//$this->call(OrganizacionSeeder::class);
-    	//$this->call(UserSeeder::class);
-        $this->call(ArticuloSeeder::class);
-        $this->call(CatastrofeSeeder::class);
-        $this->call(Centro_AcopioSeeder::class);
-        $this->call(CiudadSeeder::class);
-        $this->call(ComunaSeeder::class);
-        $this->call(DireccionSeeder::class);
-        $this->call(DonacionSeeder::class);
-        $this->call(Evento_beneficioSeeder::class);
-        $this->call(FondoSeeder::class);
-        $this->call(HistorialSeeder::class);
-        $this->call(MedidaSeeder::class);
-        $this->call(PermisoSeeder::class);
-        $this->call(ProductoSeeder::class);
+
+
+        //$this->call(OrganizacionSeeder::class);
+        //$this->call(RolSeeder::class);
+        //$this->call(UserSeeder::class);
         $this->call(RegionSeeder::class);
-        $this->call(RNV_MedidaSeeder::class);
-        $this->call(RNVSeeder::class);
-        $this->call(Rol_PermisoSeeder::class);
-        $this->call(Usuario_FondoSeeder::class);
-        $this->call(Usuario_MedidaSeeder::class);
-        $this->call(Usuario_RNVSeeder::class);
-        $this->call(Usuario_RolSeeder::class);
+        $this->call(CiudadSeeder::class);
+        $this->call(PermisoSeeder::class);
+        $this->call(ComunaSeeder::class);
+        $this->call(MedidaSeeder::class);
+        $this->call(CatastrofeSeeder::class);
         $this->call(VoluntariadoSeeder::class);
+        $this->call(RNVSeeder::class);
+        $this->call(HistorialSeeder::class);
+        $this->call(Evento_beneficioSeeder::class);
+        $this->call(ProductoSeeder::class);
+        $this->call(Centro_AcopioSeeder::class);
+        $this->call(ArticuloSeeder::class);
+        $this->call(FondoSeeder::class);
+        $this->call(DonacionSeeder::class);
+        $this->call(Usuario_RNVSeeder::class);
+        $this->call(RNV_MedidaSeeder::class);
+        $this->call(Usuario_FondoSeeder::class);
+        $this->call(Rol_PermisoSeeder::class);
+        $this->call(Usuario_MedidaSeeder::class);
+        $this->call(DireccionSeeder::class);
+        $this->call(Usuario_RolSeeder::class);
     }
 }
