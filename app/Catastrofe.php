2b6ebf30c6aa06c1401740703261544ccab85ec2
Nico <nicolas.romero.f@usach.cl>
2017-11-16 03:13:14 +0000

    fixed query

--- app/Catastrofe.php
+++ app/Catastrofe.php
@@ -7,10 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Catastrofe extends Model
 {
     protected $fillable = ['id_user','nombre','tipo','fecha','descripcion'];
-    public function getFechaAttribute($value)
+   /* public function getFechaAttribute($value)
     {
     	$arr = preg_split("-",$value);
     	$str = $arr[2] +$arr[1] +$arr[0];
         return $str;
-    }
+    }*/
 }
