22300d443a4f2f8aa639a116525a988adbce26a4
Daniel <daniel.vasquez.i@usach.cl>
2017-11-23 23:33:29 -0300

    Merge controlador Catastrofes
    Merge del controlador con los cambios para evitar errores si entra un usuario sin inicar sesión

--- app/Http/Controllers/CatastrofesController.php
+++ app/Http/Controllers/CatastrofesController.php
@@ -12,29 +12,38 @@ class CatastrofesController extends Controller
 {
 	public function index()
     {
-
-    	return view('catastrofe/catastrofeAdd');
+        if (\Auth::check()) {
+            return view('catastrofe/catastrofeAdd');
+        }
+        else {
+            return view('auth/login');
+        }
     }
 
     public function store(Request $request)
     {
-        $bool = $request->user()->authorizeRoles(['admin',]);
-        if(!$bool)
-        {
-            return view('bloqueado');
+        if (\Auth::check()) {
+            $bool = $request->user()->authorizeRoles(['admin',]);
+            if(!$bool)
+            {
+                return view('bloqueado');
+            }
+
+        	Catastrofe::create([
+        		'tipo' => $request->tipo,
+                'id_user'=> auth()->id(),
+                'nombre'=> $request->nombre,
+                'fecha' => date("m-d-Y", strtotime($request->fecha)),
+                'descripcion' => $request->descripcion,
+            ]);
+            $tweet = '#AlertaCatastrofe ' . $request->nombre . ' ' . $request->fecha;
+            Twitter::postTweet(array('status' => $tweet, 'format' => 'json'));
+            
+            return back()->with('flash','Catastrofe declarada correctamente');
         }
-
-    	Catastrofe::create([
-    		'tipo' => $request->tipo,
-            'id_user'=> auth()->id(),
-            'nombre'=> $request->nombre,
-            'fecha' => date("m-d-Y", strtotime($request->fecha)),
-            'descripcion' => $request->descripcion,
-        ]);
-        $tweet = '#AlertaCatastrofe ' . $request->nombre . ' ' . $request->fecha;
-        Twitter::postTweet(array('status' => $tweet, 'format' => 'json'));
-
-        return back()->with('flash','Catastrofe declarada correctamente');
+        else {
+            return view('auth/login');
+        }
     }
     public function historial()
     {
--- app/Role.php
+++ app/Role.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    protected $fillable = ['name','description'];
+
+    public function usuarios() {
+    	return $this->hasMany(User::class,'id_role');
+    }
+    public function users()
+	{
+	  return $this->belongsToMany(User::class);
+	}
 }
