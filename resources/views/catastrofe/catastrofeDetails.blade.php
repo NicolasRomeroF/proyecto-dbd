6e0d1d0e27e9324697a6ae67ccd38a76892cdbde
Nico <nicolas.romero.f@usach.cl>
2017-11-25 20:10:59 +0000

    boton arreglar?

--- resources/views/catastrofe/catastrofeDetails.blade.php
+++ resources/views/catastrofe/catastrofeDetails.blade.php
@@ -8,16 +8,25 @@
 
             <div class="panel-heading">Detalles de la catastrofe</div>
             <div class="panel-body">
+              <div class="form-group">
               <label class="col-md-12 control-label">Nombre de la catastrofe</label>
               <p class="col-sm-12 control-label">{{ $catastrofe->nombre }}</p>
+</div>
+              <div class="form-group">
               <label class="col-md-12 control-label">Fecha de la catastrofe</label>
               <p class="col-sm-4 control-label">{{ $catastrofe->fecha }}</p>
+</div>
+              <div class="form-group">
               <label class="col-md-12 control-label">Tipo</label>
               <p class="col-sm-4 control-label">{{ $catastrofe->tipo }}</p><br>
+</div>
+              <div class="form-group">
               <label class="col-md-12 control-label">Descripcion:</label>
               <p class="col-md-12 control-label">{{ $catastrofe->descripcion }}</p>
+</div>
+              <div class="form-group">
               <a class="btn btn-primary" href="/catastrofes/medidas/generatecentro/{{ $catastrofe->id }}">Generar centro de acopio</a>
-
+            </div>
               
 
             </div>
