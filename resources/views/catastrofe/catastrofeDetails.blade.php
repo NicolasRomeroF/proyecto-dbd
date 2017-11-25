3473b4bc0207e6b7251c0784e41d6901b47a8ffa
Nico <nicolas.romero.f@usach.cl>
2017-11-25 20:41:50 +0000

    botones bonitos :3

--- resources/views/catastrofe/catastrofeDetails.blade.php
+++ resources/views/catastrofe/catastrofeDetails.blade.php
@@ -1,40 +1,48 @@
-    @extends('layouts.app')
+      @extends('layouts.app')
 
-    @section('content')
-    <div class="container">
-      <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-          <div class="panel panel-default">
+      @section('content')
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
 
-            <div class="panel-heading">Detalles de la catastrofe</div>
-            <div class="panel-body">
-              <div class="form-group">
-              <label class="col-md-12 control-label">Nombre de la catastrofe</label>
-              <p class="col-sm-12 control-label">{{ $catastrofe->nombre }}</p>
-</div>
-              <div class="form-group">
-              <label class="col-md-12 control-label">Fecha de la catastrofe</label>
-              <p class="col-sm-12 control-label">{{ $catastrofe->fecha }}</p>
-</div>
-              <div class="form-group">
-              <label class="col-md-12 control-label">Tipo</label>
-              <p class="col-sm-12 control-label">{{ $catastrofe->tipo }}</p><br>
-</div>
-              <div class="form-group">
-              <label class="col-md-12 control-label">Descripcion:</label>
-              <p class="col-md-12 control-label">{{ $catastrofe->descripcion }}</p>
-</div>
-              <div class="form-group">
-              <a class="btn btn-primary" href="/catastrofes/medidas/generatecentro/{{ $catastrofe->id }}">Generar centro de acopio</a>
-              <a class="btn btn-primary" href="/catastrofes/medidas/generatebeneficio/{{ $catastrofe->id }}">Generar evento a beneficio</a>
-              <a class="btn btn-primary" href="/catastrofes/medidas/generatedonacion/{{ $catastrofe->id }}">Generar donación</a>
-              <a class="btn btn-primary" href="/catastrofes/medidas/generatevoluntariado/{{ $catastrofe->id }}">Generar voluntariado</a>
-            </div>
-              
+              <div class="panel-heading">Detalles de la catastrofe</div>
+              <div class="panel-body">
+                <div class="form-group">
+                  <label class="col-md-12 control-label">Nombre de la catastrofe</label>
+                  <p class="col-sm-12 control-label">{{ $catastrofe->nombre }}</p>
+                </div>
+                <div class="form-group">
+                  <label class="col-md-12 control-label">Fecha de la catastrofe</label>
+                  <p class="col-sm-12 control-label">{{ $catastrofe->fecha }}</p>
+                </div>
+                <div class="form-group">
+                  <label class="col-md-12 control-label">Tipo</label>
+                  <p class="col-sm-12 control-label">{{ $catastrofe->tipo }}</p><br>
+                </div>
+                <div class="form-group">
+                  <label class="col-md-12 control-label">Descripcion:</label>
+                  <p class="col-md-12 control-label">{{ $catastrofe->descripcion }}</p>
+                </div>
+                <label class="col-md-12 control-label">Generar: </label>
+                <table class="table table-sm">
+                  <tbody>
+                    <tr>
+                      <th scope="col"><a class="btn btn-primary" href="/catastrofes/medidas/generatecentro/{{ $catastrofe->id }}">Centro de acopio</a></th>
+                      <th scope="col"><a class="btn btn-primary" href="/catastrofes/medidas/generatebeneficio/{{ $catastrofe->id }}">Evento a beneficio</a></th>
+                      <th scope="col"><a class="btn btn-primary" href="/catastrofes/medidas/generatedonacion/{{ $catastrofe->id }}">Donación</a>
+                      </th>
+                      <th scope="col"><a class="btn btn-primary" href="/catastrofes/medidas/generatevoluntariado/{{ $catastrofe->id }}">Voluntariado</a>
+                      </th>
 
+                    </tr>
+                  </tbody>
+                </table>
+                
+
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  @endsection
+      @endsection
