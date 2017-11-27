d5b76024c2f01c04176d658e318a243fc4b3e4ad
Daniel <daniel.vasquez.i@usach.cl>
2017-11-27 02:35:42 -0300

    Implementación RNV
    Implementación del listado de voluntarios, realizado con ayuda de Verónica

---
+++ resources/views/RNV/listado.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+		    <div class="row">
+		    		        <div class="col-md-8 col-md-offset-2">
+		    		        	<div class="panel panel-default">
+                        <div class="panel-heading">Registro Nacional de Voluntarios</div>
+                        <div class="col-md-4 text-center"> 
+                          <a href="/RNV/voluntariosDisponibles" class="btn btn-info">Ver voluntarios disponibles</a>
+                        </div>
+<table class="table table-sm">
+  <thead>
+    <tr>
+      <th scope="col">Estado</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Apellido</th>
+      <th scope="col">Detalles</th>
+    </tr>
+  </thead>
+  <tbody>
+  	@foreach($voluntarios as $voluntario)
+    <tr>
+      @if($voluntario->disponible)
+      <td>Disponible</td>
+      @else
+      <td>Ocupado</td>
+      @endif
+      <td>{{ $voluntario->nombre }}</td>
+      <td>{{ $voluntario->apellido }}</td>
+      <td><a href="/RNV/voluntario/{{$voluntario->id}}">Ver detalles</a></td>
+    </tr>
+    @endforeach  
+  </tbody>
+</table>
+</div>
+</div>
+</div>
+</div>
+@endsection
